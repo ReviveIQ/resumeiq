de36f749fdec384b7ad3a06b4b856a763ecfbfcb
Manager <bryan.greer1@gmail.com>
2026-05-24 23:19:20 -0400

    feat: add /admin/pipeline and /admin/stripe routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { Switch, Route } from "wouter";
 import ResumeIQ from "./pages/ResumeIQ";
+import PipelineTracker from "./pages/PipelineTracker";
+import StripeDashboard from "./pages/StripeDashboard";
 
 export default function App() {
-  return <ResumeIQ />;
+  return (
+    <Switch>
+      <Route path="/admin/pipeline" component={PipelineTracker} />
+      <Route path="/admin/stripe" component={StripeDashboard} />
+      <Route component={ResumeIQ} />
+    </Switch>
+  );
 }
