e0bace273ee38f4e9e76cbae0366e5ee9ced6b7a
Manager <bryan.greer1@gmail.com>
2026-05-28 22:31:23 -0400

    Implement tracking for page views and attribution
    Added tracking for page views and attribution.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import { Switch, Route } from "wouter";
+import { useEffect } from "react";
 import ResumeIQ from "./pages/ResumeIQ";
 import LandingPage from "./pages/LandingPage";
 import PipelineTracker from "./pages/PipelineTracker";
 import StripeDashboard from "./pages/StripeDashboard";
+import { trackEvent, captureAttribution } from "./tracking";
 
 export default function App() {
+  useEffect(() => {
+    captureAttribution();
+    trackEvent('page_view', { path: window.location.pathname });
+  }, []);
+
   return (
     <Switch>
       <Route path="/" component={LandingPage} />
