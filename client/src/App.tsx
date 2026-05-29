824221c59dc4a548de860e3c836f2bd1c33fd8ec
Manager <bryan.greer1@gmail.com>
2026-05-28 20:14:37 -0400

    feat: add landing page route, move app to /app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import { Switch, Route } from "wouter";
 import ResumeIQ from "./pages/ResumeIQ";
+import LandingPage from "./pages/LandingPage";
 import PipelineTracker from "./pages/PipelineTracker";
 import StripeDashboard from "./pages/StripeDashboard";
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/" component={LandingPage} />
+      <Route path="/app" component={ResumeIQ} />
       <Route path="/admin/pipeline" component={PipelineTracker} />
       <Route path="/admin/stripe" component={StripeDashboard} />
-      <Route component={ResumeIQ} />
     </Switch>
   );
 }
